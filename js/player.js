b1a104d1b91d63d3614acb6b699edbbce6607e8b
nurali-code <nur3.dav.97@gmail.com>
2025-02-03 16:56:37 +0500

    piano done

--- js/player.js
+++ js/player.js
@@ -1,74 +1,84 @@
 $(document).ready(function () {
-    const audio = $('.audio')[0];
-    let isPlaying = false;
+    // Итерируем по каждому плееру на странице
+    $('.player').each(function () {
+        const player = $(this);
+        const audio = player.find('.audio')[0];
+        const playlist = player.find('.playlist li');
+        let isPlaying = false;
+        let currentTrack = 0;
 
-    function loadTrack(index) {
-        const track = $('.playlist li').eq(index);
-        $('.playlist li').removeClass('is_active');
-        track.addClass('is_active');
-        audio.src = track.data('src');
-        $('.current-name').text(track.find('.s-name').text());
-        $('.current-author').text(track.find('.s-author').text());
-        $('.current-cover').attr('src', track.find('.s-cover').attr('src'));
-        $('.player-download').attr('download', track.find('.s-author').text() + ' ' + track.find('.s-name').text());
-        $('.player-download').attr('href', track.data('src'));
-        audio.load();
-        if (isPlaying) audio.play();
-    }
-
-    function togglePlay() {
-        if (audio.paused) {
-            audio.play();
-            isPlaying = true;
-            $('.player-pause').addClass('is_active');
-        } else {
-            audio.pause();
-            isPlaying = false;
-            $('.player-pause').removeClass('is_active');
+        function loadTrack(index) {
+            const track = playlist.eq(index);
+            playlist.removeClass('is_active');
+            track.addClass('is_active');
+            audio.src = track.data('src');
+            player.find('.current-name').text(track.find('.s-name').text());
+            player.find('.current-author').text(track.find('.s-author').text());
+            player.find('.current-cover').attr('src', track.find('.s-cover').attr('src'));
+            player.find('.player-download').attr('download', track.find('.s-author').text() + ' ' + track.find('.s-name').text());
+            player.find('.player-download').attr('href', track.data('src'));
+            audio.load();
+            if (isPlaying) {
+                audio.play();
+                player.find('.playlist li use').attr('xlink:href', '#play');
+                player.find('.player-pause use, .playlist li.is_active use').attr('xlink:href', '#pause');
+            }
         }
-    }
 
-    function updateProgress(e) {
-        const { currentTime, duration } = e.target;
-        const percent = (currentTime / duration) * 100;
-        $('.progress-bar span').css('width', `${percent}%`);
-    }
+        function togglePlay() {
+            if (audio.paused) {
+                audio.play();
+                isPlaying = true;
+                player.find('.playlist li use').attr('xlink:href', '#play');
+                player.find('.player-pause use, .playlist li.is_active use').attr('xlink:href', '#pause');
+            } else {
+                audio.pause();
+                player.find('.player-pause use, .playlist li.is_active use').attr('xlink:href', '#play');
+                isPlaying = false;
+            }
 
-    function setProgress(e) {
-        const progressWidth = $(this).width();
-        const offsetX = e.offsetX;
-        const duration = audio.duration;
-        audio.currentTime = (offsetX / progressWidth) * duration;
-    }
+        }
 
-    let currentTrack = 0;
-    loadTrack(currentTrack);
+        function updateProgress(e) {
+            const { currentTime, duration } = e.target;
+            const percent = (currentTime / duration) * 100;
+            player.find('.progress-bar span').css('width', `${percent}%`);
+        }
 
-    $('.player-pause').click(togglePlay);
+        function setProgress(e) {
+            const progressWidth = $(this).width();
+            const offsetX = e.offsetX;
+            const duration = audio.duration;
+            audio.currentTime = (offsetX / progressWidth) * duration;
+        }
 
-    $('.player-prev').click(function () {
-        currentTrack = (currentTrack > 0) ? currentTrack - 1 : $('.playlist li').length - 1;
+        // Загрузка первого трека
         loadTrack(currentTrack);
-    });
 
-    $('.player-next').click(function () {
-        currentTrack = (currentTrack < $('.playlist li').length - 1) ? currentTrack + 1 : 0;
-        loadTrack(currentTrack);
-    });
+        // Обработчики событий для каждого плеера
+        player.find('.player-pause').click(togglePlay);
 
-    $('.playlist li').click(function () {
-        currentTrack = $(this).index();
-        loadTrack(currentTrack);
-        audio.play();
-        isPlaying = true;
-        $('.player-pause').addClass('is_active');
-    });
+        player.find('.player-prev').click(function () {
+            currentTrack = (currentTrack > 0) ? currentTrack - 1 : playlist.length - 1;
+            loadTrack(currentTrack);
+        });
 
-    audio.addEventListener('timeupdate', updateProgress);
+        player.find('.player-next').click(function () {
+            currentTrack = (currentTrack < playlist.length - 1) ? currentTrack + 1 : 0;
+            loadTrack(currentTrack);
+        });
 
-    $('.progress-bar').click(setProgress);
+        player.find('.playlist li').click(function () {
+            currentTrack = $(this).index();
+            loadTrack(currentTrack);
+        });
 
-    audio.addEventListener('ended', function () {
-        $('.player-next').click();
+        audio.addEventListener('timeupdate', updateProgress);
+
+        player.find('.progress-bar').click(setProgress);
+
+        audio.addEventListener('ended', function () {
+            player.find('.player-next').click();
+        });
     });
 });
